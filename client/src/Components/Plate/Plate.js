5960c9003793494df67793a5ff611ec8b5e1959d
heroserth2 <59160969@go.buu.ac.th>
2019-02-12 10:04:38 +0700

    chang name Style my.css --> colorStyle.css

--- client/src/Components/Plate/Plate.js
+++ client/src/Components/Plate/Plate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import '../../StyleSheets/my.css';
+import '../../StyleSheets/colorStyle.css';
 class Plate extends Component {
     render() {
         return (
