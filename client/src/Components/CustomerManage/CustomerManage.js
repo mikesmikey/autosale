d017680cab382b1d447832283391a12f06790d18
Kraiwich Kornsri <42442245+mikesmikey@users.noreply.github.com>
2019-02-15 01:11:25 +0700

    fixing file name bug.

--- client/src/Components/CustomerManage/CustomerManage.js
+++ client/src/Components/CustomerManage/CustomerManage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import '../../StyleSheets/my.css';
+import '../../StyleSheets/colorStyle.css';
 class CustomerManage extends Component {
     render() {
         return (
