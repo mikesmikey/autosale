909e19a70cf2e2d3f9edee028f161b076366ab79
Kraiwich <le_kraiwich@hotmail.com>
2019-03-29 15:32:28 +0700

    using standard JS code in server side.

--- server/WebDAO.js
+++ server/WebDAO.js
@@ -1,149 +1,143 @@
-const mongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
-const url = 'mongodb://hanami:hanami02@ds131765.mlab.com:31765/ooad_kob';
-const dbName = 'ooad_kob';
+const mongoClient = require('mongodb').MongoClient
+const url = 'mongodb://hanami:hanami02@ds131765.mlab.com:31765/ooad_kob'
+const dbName = 'ooad_kob'
 
 class WebDAO {
+  /* ===========[User DAO]=================== */
 
-    /*===========[User DAO]===================*/
+  getAllUser () {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').find({}).project({ '_id': 0, 'password': 0 }).toArray((err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 
-    getAllUser() {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').find({}).project({ "_id": 0, "password": 0 }).toArray((err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
+  getUserByUsername (username) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').findOne({ 'username': username }, { '_id': 0, 'password': 0 }, (err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 
-    getUserByUsername(username) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').findOne({ "username": username }, { "_id": 0, "password": 0 }, (err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
+  insertUser (user) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').findOne({ 'username': user.username }, (err, data) => {
+          if (err) { throw err }
+          if (!data) {
+            db.collection('User').insertOne(user, (err, result) => {
+              if (err) { throw err }
+              return resolve(true)
+            })
+          } else { return resolve(false) }
+        })
+      })
+    })
+  }
 
-    insertUser(user) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').findOne({ "username": user.username }, (err, data) => {
-                    if (err) { throw err }
-                    if (!data) {
-                        db.collection('User').insertOne(user, (err, result) => {
-                            if (err) { throw err }
-                            return resolve(true);
-                        });
-                    } else { return resolve(false) }
-                });
-            });
-        });
-    }
+  insertManyUsers (users) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').insertMany(users, (err, result) => {
+          if (err) { throw err }
+          return resolve(true)
+        })
+      })
+    })
+  }
 
-    insertManyUsers(users) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').insertMany(users, (err, result) => {
-                    if (err) { throw err }
-                        return resolve(true);
-                });
-            });
-        });
-    }
+  editUser (newUserData) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').findOneAndUpdate({ 'username': newUserData.username }, { '$set': newUserData }, (err, result) => {
+          if (err) { throw err }
+          if (result.value) {
+            return resolve(true)
+          } else { return resolve(false) }
+        })
+      })
+    })
+  }
 
+  deleteUserByUsername (username) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').findOne({ 'username': username }, (err, data) => {
+          if (err) { throw err }
+          if (data) {
+            db.collection('User').deleteOne({ username }, (err, result) => {
+              if (err) { throw err }
+              return resolve(true)
+            })
+          } else { return resolve(false) }
+        })
+      })
+    })
+  }
 
-    editUser(newUserData) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').findOneAndUpdate({ "username": newUserData.username }, { "$set": newUserData }, (err, result) => {
-                    if (err) { throw err }
-                    if (result.value) {
-                        return resolve(true);
-                    } else { return resolve(false) }
-                });
-            });
-        });
-    }
+  getAllUserByType (type) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').find({ 'typeOfUser': type }).limit(16).project({ '_id': 0, 'password': 0 }).toArray((err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 
-    deleteUserByUsername(username) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').findOne({ "username": username }, (err, data) => {
-                    if (err) { throw err }
-                    if (data) {
-                        db.collection('User').deleteOne({ username }, (err, result) => {
-                            if (err) { throw err }
-                            return resolve(true);
-                        });
-                    } else { return resolve(false) }
-                });
-            });
-        });
-    }
+  getAllUserByTypeAndUsername (type, username) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        const regex = new RegExp(`${username}`)
+        db.collection('User').find({ 'username': regex, 'typeOfUser': type }).limit(16).project({ '_id': 0, 'password': 0 }).toArray((err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 
-    getAllUserByType(type) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').find({ "typeOfUser": type }).limit(16).project({ "_id": 0, "password": 0 }).toArray((err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
+  getAllFaculty () {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('Faculty').find({}).toArray((err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 
-    getAllUserByTypeAndUsername(type, username) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                const regex = new RegExp(`${username}`);
-                db.collection('User').find({ "username": regex, "typeOfUser": type }).limit(16).project({ "_id": 0, "password": 0 }).toArray((err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
-
-    getAllFaculty() {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('Faculty').find({}).toArray((err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
-
-    /*===========[Score DAO]===================*/
-    getScoreByUsername(username) {
-        return new Promise((resolve, reject) => {
-            mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                const db = client.db(dbName)
-                db.collection('User').findOne({ "username": username }, { "_id": 0, "password": 0 }, (err, data) => {
-                    if (err) { throw err }
-                    return resolve(data);
-                });
-            });
-        });
-    }
+  /* ===========[Score DAO]=================== */
+  getScoreByUsername (username) {
+    return new Promise((resolve, reject) => {
+      mongoClient.connect(url, { useNewUrlParser: true }, (_err, client) => {
+        const db = client.db(dbName)
+        db.collection('User').findOne({ 'username': username }, { '_id': 0, 'password': 0 }, (err, data) => {
+          if (err) { throw err }
+          return resolve(data)
+        })
+      })
+    })
+  }
 }
 
-
-
-
-module.exports = WebDAO;
+module.exports = WebDAO
