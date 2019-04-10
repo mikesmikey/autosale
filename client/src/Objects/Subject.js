60415c1268f68c7b5b99bd57d63b4d39b8822d96
ChainarongChantian <houstenbosh@hotmail.com>
2019-04-10 15:19:16 +0700

    new Bie

--- client/src/Objects/Subject.js
+++ client/src/Objects/Subject.js
@@ -2,8 +2,8 @@ class Subject {
   constructor (form) {
     this.facultyId = form.facultyId ? form.facultyId : 0
     this.branchId = form.branchId ? form.branchId : 0
-    this.subjectId = form.subject_id ? form.subject_id : ''
-    this.subjectName = form.subject_name ? form.subject_name : ''
+    this.subjectId = form.subjectId ? form.subjectId : ''
+    this.subjectName = form.subjectName ? form.subjectName : ''
     this.credits = form.credits ? form.credits : 0
   }
 }
