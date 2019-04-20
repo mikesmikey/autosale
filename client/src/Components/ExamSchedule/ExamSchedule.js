a4d79f3f8d1b339ceed59b708796643a74f43f60
Kraiwich Kornsri <42442245+mikesmikey@users.noreply.github.com>
2019-04-20 22:45:36 +0700

    fix user manage array bug.

--- client/src/Components/ExamSchedule/ExamSchedule.js
+++ client/src/Components/ExamSchedule/ExamSchedule.js
@@ -1,12 +1,114 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
+import ClientService from '../Utilities/ClientService'
 
 // Components
 import Modal from '../Utilities/Modal'
 
 import '../../StyleSheets/examSchedule.css'
 
+const CServiceObj = new ClientService()
+
 class ExamSchedule extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      exams: [],
+      courses: [],
+      selectedExam: null
+    }
+  }
+
+  componentDidMount () {
+    this.loadUserExam()
+    this.loadUserCourse()
+  }
+
+  componentDidUpdate (prevProps, prevStates) {
+    if (this.state.exams !== prevStates.exams || this.state.courses !== prevStates.courses) {
+      // this.renderUserExam()
+    }
+  }
+
+  loadUserExam () {
+    if (this.props.user) {
+      console.log('LOG: Loading user exam')
+      for (let i = 0, p = Promise.resolve(); i < this.props.user.courses.length; i++) {
+        p = p.then((_) => new Promise(resolve => {
+          CServiceObj.getAllExamBySubjectAndCourse(this.props.user.courses[i].subjectId, this.props.user.courses[i].courseId).then((result) => {
+            let newExams = this.state.exams
+            newExams = newExams.concat(result)
+            console.log(newExams)
+            this.setState({
+              exams: newExams
+            })
+            resolve()
+          })
+        }))
+      }
+    }
+  }
+
+  loadUserCourse () {
+    for (let i = 0, p = Promise.resolve(); i < this.props.user.courses.length; i++) {
+      p = p.then((_) => new Promise(resolve => {
+        CServiceObj.getCourseByIdAndSubjectId(this.props.user.courses[i].courseId, this.props.user.courses[i].subjectId).then((result) => {
+          let newCourses = this.state.courses
+          newCourses = newCourses.concat(result)
+          this.setState({
+            courses: newCourses
+          })
+          resolve()
+        })
+      }))
+    }
+  }
+
+  renderUserExam () {
+    let examItems = []
+    if (this.state.exams.length > 0 && this.state.courses.length > 0) {
+      for (let i = 0; i < this.state.exams.length; i++) {
+        if (this.state.exams[i].examConfirm) {
+          this.state.exams[i].rooms.forEach(room => {
+            for (let j = 0; j < room.examSeats.length; j++) {
+              let seatFound = room.examSeats[j].find((seat) => {
+                return seat.studentCode === this.props.user.username
+              })
+              if (seatFound) {
+                let examData = {}
+                examData.subjectId = this.state.exams[i].subjectId
+                examData.subjectName = this.state.exams[i].subjectName
+                let courseFound = this.state.courses.find((course) => {
+                  return course.subjectId === this.state.exams[i].subjectId && course.courses[0].courseId === this.state.exams[i].courseId
+                })
+                if (courseFound) {
+                  examData.school_year = courseFound.courses[0].school_year
+                  examData.semester = courseFound.courses[0].semester
+                } else {
+                  examData.school_year = 'unknown'
+                  examData.semester = 'unknown'
+                }
+                examData.examName = this.state.exams[i].examName
+                examData.roomId = room.roomId
+                examData.seatNumber = seatFound.seatNumber
+                examData.date = this.state.exams[i].date
+                console.log(examData)
+                examItems.push(
+                  <ExamScheduleItem
+                    key={this.state.exams[i]._id}
+                    showModal={() => { this.extendDetailModal.showModal() }}
+                    examData={examData}
+                  />
+                )
+              }
+            }
+          })
+        }
+      }
+    }
+    return examItems
+  }
+
   createSampleData () {
     var returnArr = []
     for (var i = 0; i < 50; i++) {
@@ -21,7 +123,7 @@ class ExamSchedule extends Component {
       <div className="subcontent-main-div exam-schedule">
         <Modal ref={instance => { this.extendDetailModal = instance }} content={<ExamExtendDetail closeModal={() => { this.extendDetailModal.closeModal() }} />} />
         <div className="schedule-area box is-round">
-          {this.createSampleData()}
+          {this.renderUserExam()}
         </div>
       </div>
     )
@@ -29,61 +131,33 @@ class ExamSchedule extends Component {
 }
 
 class ExamScheduleItem extends Component {
-  // render () {
-  //   return (
-  //     <div className="exam-schedule-item box is-round" onClick={this.props.showModal}>
-  //       <div className="columns">
-  //         <div className="exam-date-box column is-2 is-white-violet">
-  //           <span className="date-labels">
-  //             <h1>99</h1>
-  //             <h3>ธันวาคม</h3>
-  //             <h3>99999</h3>
-  //             <h2>เวลา 99:99</h2>
-  //           </span>
-  //         </div>
-  //         <div className="exam-detail-column column">
-  //           <div className="exam-title">
-  //             <div className="columns">
-  //               <div className="column auto">
-  //                 <h2 id="subject-id">88624459 : </h2>
-  //               </div>
-  //               <div className="column">
-  //                 <h2 id="subject-name">Object-Oriented Analysis and Design</h2>
-  //               </div>
-  //             </div>
-  //           </div>
-  //           <div className="exam-detail">
-  //             <div className="box is-round is-light-gray">
-  //               <p>กลุ่มที่ : 1 ห้องสอบ : IF-404 เลขที่นั่งสอบ : 12 ประเภท : กลางภาค</p>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // }
   render () {
+    const monthNames = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน',
+      'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'
+    ]
+    const examDate = new Date(this.props.examData.date)
+    console.log(new Date(this.props.examData.date).getMonth())
     return (
       <div className="exam-schedule-item box is-round" onClick={this.props.showModal}>
         <div className="columns">
           <div className="exam-date-box column is-2 is-white-violet">
             <div className="date-labels">
-              <h1>99</h1>
-              <h3>ธันวาคม</h3>
-              <h3>99999</h3>
-              <h2>เวลา 99:99</h2>
+              <h1>{examDate.getDate()}</h1>
+              <h3>{monthNames[examDate.getMonth()]}</h3>
+              <h3>{examDate.getFullYear()}</h3>
+              <h2>เวลา 99:99 - 99:99</h2>
             </div>
           </div>
           <div className="exam-detail-column column">
-            <h2 id="subject-name">ชื่อการสอบ</h2>
+            <h2 id="subject-name">{this.props.examData.examName}</h2>
             <div className="exam-detail box is-round is-light-gray">
               <div className="small-detail-box">
-                <p>รหัสวิชา : ชื่อวิชา</p>
-                <p>ปีการศึกษา เทอม </p>
+                <p>วิชา: {this.props.examData.subjectId} - {this.props.examData.subjectName}</p>
+                <p>ปีการศึกษา {this.props.examData.school_year} เทอม {this.props.examData.semester}</p>
               </div>
               <div className="small-detail-box">
-                <p>ตึก ห้องสอบ</p>
-                <p>เลขที่นั่งสอบ : 12A</p>
+                <p>ห้องสอบ: {this.props.examData.roomId}</p>
+                <p>เลขที่นั่งสอบ : {this.props.examData.seatNumber}</p>
               </div>
             </div>
           </div>
--- client/src/Components/MainScreen/MainScreen.js
+++ client/src/Components/MainScreen/MainScreen.js
@@ -38,7 +38,9 @@ class MainScreen extends Component {
             <Home />
           } />
           <Route path="/exam_schedule" render={(props) =>
-            <ExamSchedule />
+            <ExamSchedule
+              user={this.props.user}
+            />
           } />
           <Route path="/exam_create" render={(props) =>
             <ExamCreateScreen />
