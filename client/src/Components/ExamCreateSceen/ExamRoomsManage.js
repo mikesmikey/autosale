2b2b447e6a34796eb82e527114849464f167e31f
book <bookploy1928@hotmail.com>
2019-05-17 15:37:56 +0700

    fix title.

--- client/src/Components/ExamCreateSceen/ExamRoomsManage.js
+++ client/src/Components/ExamCreateSceen/ExamRoomsManage.js
@@ -247,7 +247,7 @@ class ExamRoomsModal extends Component {
     return (
       <div className="exam-rooms-modal box with-title">
         <div className="box-title is-violet">
-          <h3 className="label is-2">เพิ่มห้องสอบ</h3>
+          <h3 className="label is-2">จัดการห้องสอบ</h3>
           <button className="exit-button fas fa-times fa-1x" onClick={this.props.closeModal}></button>
         </div>
         <div className={`box-content ${this.state.isLoading ? 'disabled' : ''}`}>
