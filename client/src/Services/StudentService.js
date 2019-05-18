bf1619e3e1b0f8edb57aade0fc336a26b2707f17
Kraiwich Kornsri <42442245+mikesmikey@users.noreply.github.com>
2019-05-19 05:50:01 +0700

    Revert "Fluke"

--- client/src/Services/StudentService.js
+++ client/src/Services/StudentService.js
@@ -1,19 +1,11 @@
 import axios from 'axios'
 class StudentService {
-  addManyStudents(users) {
-    return new Promise((resolve, reject) => {
-      axios.post(`/user/addmany`, { 'usersData': users }).then((result) => {
-        resolve(result.data)
-      })
-    })
-  }
-
-  getAllUserByStudentType() {
-    return new Promise((resolve, reject) => {
-      axios.get(`/user/type/student`).then((result) => {
-        resolve(result.data)
-      })
-    })
-  }
+    addManyStudents (users) {
+        return new Promise((resolve, reject) => {
+          axios.post(`/user/addmany`, { 'usersData': users }).then((result) => {
+            resolve(result.data)
+          })
+        })
+      }
 }
 export default StudentService
